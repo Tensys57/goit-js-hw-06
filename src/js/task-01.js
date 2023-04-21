2675b4e90965a75ddbd08ef4ac35a61b3e201829
Tensys57 <tensys57@gmail.com>
2023-04-21 23:54:21 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -4,11 +4,9 @@
 // Для кожного элемента li.item у списку ul#categories, знайде і виведе в консоль текст заголовку елемента (тегу <h2>) і кількість елементів в категорії (усіх <li>, вкладених в нього).
 // Для виконання цього завдання потрібно використати метод forEach() і властивості навігації по DOM.
 
-const ulQuantity = document.querySelectorAll("li.item").length;
-console.log("Number of categories:", ulQuantity);
-const list = document.querySelectorAll("#categories");
-console.log("list :>> ", list);
+const list = document.querySelectorAll(".item");
+console.log("Number of categories:", list.length);
 list.forEach((el) => {
-  const title = el.textContent;
-  console.log(title);
+  console.log("Category: ", el.querySelector("h2").textContent);
+  console.log("Elements:", el.querySelectorAll("li").length);
 });
